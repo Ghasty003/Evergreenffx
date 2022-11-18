a751dd8d129c3a12856377559ac904c21a22a31c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-11-18 02:18:29 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../src/styles/App.scss'
+import Side from './components/Side'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Side />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Side.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import illustration from "../assets/Illustration.png";
+
+
+function Side() {
+    return (
+        <div class="include">
+            <img src={illustration} alt="Illustration" />
+            <h2>Smart Trading Technology</h2>
+            <p>EvergreenFFx intelligent bot eliminates all the stress and hassle of traditional forex
+                forex trading
+            </p>
+        </div>
+    );
+}
+
+export default Side;
