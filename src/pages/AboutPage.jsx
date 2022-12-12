e782dd5c3e009c351b7437e31428b9abdc8a2a2b
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-12 03:51:55 -0800

    Completed TopPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
+import TopPage from '../components/TopPage';
 
 function AboutPage() {
     return (
         <div>
             <Nav />
-
+            <TopPage headerText="About us" />
             <Footer />
         </div>
     );
