29e27ee30005ca70a2af160923a6a9708820d956
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-12 12:12:16 -0800

    Initialized ContactPage

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+
+function ContactPage() {
+    return (
+        <div>
+            <Nav />
+
+            <Footer />
+        </div>
+    );
+}
+
+export default ContactPage;
