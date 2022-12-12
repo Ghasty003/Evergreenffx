b7c710a928ba5e7fefb69d2556acea3cbba763c0
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-11 18:54:25 -0800

    Completed Navnar responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styles/nav.scss";
 import logo from "../assets/home-logo.png";
+import logoM from "../assets/ever2 1.png";
 import { Link, useMatch, useNavigate, useResolvedPath } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 const CustomLink = ({to, text}) => {
     const resolvedPath = useResolvedPath(to);
@@ -14,11 +17,24 @@ const CustomLink = ({to, text}) => {
 function Nav() {
 
     const navigate = useNavigate();
+    const ul = useRef();
+
+    const hide = () => {
+        if (window.innerWidth <= 980) {
+            ul.current.style.top = "-1000px"
+        }
+    }
+
+    const show = () => {
+        ul.current.style.top = 0;
+    }
 
     return (
         <nav>
             <img src={logo} alt="logo" />
-            <ul>
+            <img src={logoM} alt="logo" />
+            <ul ref={ul}>
+                <FaTimes onClick={hide} className="times" size={30} />
                 <CustomLink to="/" text="Home" />
                 <CustomLink to="/about" text="About" />
                 <CustomLink to="/contact" text="Contact" />
@@ -26,9 +42,10 @@ function Nav() {
                 <CustomLink to="t&c" text="T&Cs" />
                 <CustomLink to="/privacy" text="Privacy" />
             </ul>
-            <div>
+            <div onClick={show}>
                 <button onClick={() => navigate("/login")}>Login</button>
                 <button onClick={() => navigate("/create")}>Get Started</button>
+                <FaBars className="bars" size={30} />
             </div>
         </nav>
     );
